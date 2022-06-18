9842f920cbcca4783efcd2ee563bd5da640c7cc4
sanjay <ssah6694@gmail.com>
2022-06-18 11:30:58 +0530

    [sanjay]Added:updated in UI design.

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,9 +1,10 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { NgModule, Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
+ 
 
   transform(value: any, args: any): any {
     if(!args){
@@ -17,4 +18,7 @@ export class FilterPipe implements PipeTransform {
     })
   }
 
+  
+
 }
+
